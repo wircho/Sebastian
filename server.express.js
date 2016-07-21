9795809d989a14ddc3f6f70a1078a2e48fdb7e2c
AdolfoX Rodriguez <adolfox.rodriguez@intel.com>
2016-07-21 12:57:22 -0400

    completely removed mongo

--- server.express.js
+++ server.express.js
@@ -2,12 +2,8 @@ import express from 'express';
 var bodyParser 				= require('body-parser');
 var cookieParser 			= require('cookie-parser');
 var session      			= require('express-session');
-var mongoose 				= require('mongoose');
-var MongoStore 				= require('connect-mongo')(session);
 var moment 					= require('moment');
 const app 					= express();
-const MONGODB_URI 			= fallback(process.env.MONGODB_URI,"mongodb://localhost:27017/my-db");
-const MONGODB_SECRET		= fallback(process.env.MONGODB_SECRET,"uy7gn7gn78g7");
 
 //Utilities
 function def(x) {
@@ -98,45 +94,13 @@ function projff() {
   }
 }
 
-//Time constants
-var ONE_MINUTE = 1000*60;
-var TWO_MINUTES = ONE_MINUTE*2;
-var HALF_HOUR = ONE_MINUTE*30;
-var ONE_HOUR = ONE_MINUTE*60;
-var ONE_DAY = ONE_HOUR*24;
-var ONE_WEEK = ONE_DAY*7;
-var TWO_WEEKS = ONE_WEEK*2;
-var ONE_YEAR = ONE_DAY*365;
-
-//Database+Session
-mongoose.connect(MONGODB_URI, function(error) {
-	if (error) {
-		console.log("Error connecting to Mongo:");
-		console.log(error);
-	}
-});
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(session({
-    cookie: { maxAge: ONE_YEAR } ,
-    secret: MONGODB_SECRET ,
-    resave: true,
-    saveUninitialized: true,
-    store:new MongoStore({
-    	mongooseConnection: mongoose.connection,
-        collection: 'session', 
-        auto_reconnect:true
-    })
-}));
-
 //Babel+Webpack
 app.use('/', express.static('public'));
 
 
 //api
-app.get('/api', function (req, res) {
-
-});
+// app.get('/api', function (req, res) {
+//
+// });
 
 app.listen(process.env.PORT || 8080);
