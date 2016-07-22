cfb9e4a4b6357124a2e454def46d7e58d24e1890
AdolfoX Rodriguez <adolfox.rodriguez@intel.com>
2016-07-21 21:25:59 -0400

    forcing https properly

--- server.express.js
+++ server.express.js
@@ -7,7 +7,7 @@ const app 					= express();
 
 //HTTP->HTTPS Redirect
 app.use(function(req, res, next) {
-	if (req.protocol !== "https" || req.headers.host.indexOf("localhost") === 0 || req.headers.host.indexOf("127.0.0.1") === 0) {
+	if (req.protocol === "https" || req.headers.host.indexOf("localhost") === 0 || req.headers.host.indexOf("127.0.0.1") === 0) {
 		next();
 	}else {
 		var url = 'https://' + req.headers.host + req.url;
