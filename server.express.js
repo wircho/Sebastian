aca1936bbd4aca027635b3a5ed0be7cc710a2c2c
AdolfoX Rodriguez <adolfox.rodriguez@intel.com>
2016-07-21 21:47:59 -0400

    removed redirection logs

--- server.express.js
+++ server.express.js
@@ -10,13 +10,10 @@ const app 					= express();
 //HTTP->HTTPS Redirect
 app.use(function(req, res, next) {
 	var secure = req.headers['x-forwarded-proto'] === "https";
-	console.log("secure connection: " + secure);
 	if (secure || req.headers.host.indexOf("localhost") === 0 || req.headers.host.indexOf("127.0.0.1") === 0) {
 		next();
 	}else {
-		var url = 'https://' + req.headers.host + req.url;
-		console.log("redirecting to: " + url);
-		res.redirect(url);
+		res.redirect('https://' + req.headers.host + req.url);
 	}
 });
 
